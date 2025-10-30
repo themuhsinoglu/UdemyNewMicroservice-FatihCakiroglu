2b63bc0aa933a3e82626ee5002ccfa38493fa17d
ahmet.muhsinolgu <ahmet.muhsinoglu@madamecoco.com>
2025-10-30 16:11:48 +0300

    Add seeding and versioning to API
    Updated `Program.cs` to include namespaces for `Categories` and `Courses` features, and registered new services for API versioning and data seeding. Introduced `SeedData.cs` to seed initial categories and courses into the database. Added endpoints for categories and courses with versioning support. Managed database transactions manually during seeding.

--- UdemyNewMicroservice.Catalog.Api/Program.cs
+++ UdemyNewMicroservice.Catalog.Api/Program.cs
@@ -1,4 +1,6 @@
 using UdemyNewMicroservice.Catalog.Api;
+using UdemyNewMicroservice.Catalog.Api.Features.Categories;
+using UdemyNewMicroservice.Catalog.Api.Features.Courses;
 using UdemyNewMicroservice.Catalog.Api.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +10,18 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddOptionsExt();
 builder.Services.AddDatabaseServiceExt();
 builder.Services.AddCommonServiceExt(typeof(CatalogAssembly));
+builder.Services.AddVersioningExt();
 
 var app = builder.Build();
+
+await app.AddSeedDataExt().ContinueWith(x =>
+{
+    Console.WriteLine(x.IsFaulted ? x.Exception?.Message : "Seed data has been saved successfully");
+});
+
+app.AddCategoryGroupEndpointExt(app.AddVersionSetExt());
+app.AddCourseGroupEndpointExt(app.AddVersionSetExt());
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
