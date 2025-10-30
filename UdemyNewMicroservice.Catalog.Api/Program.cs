060ed92d0dd1da03d8f6b9e85afd751005da56e6
ahmet.muhsinolgu <ahmet.muhsinoglu@madamecoco.com>
2025-10-30 14:04:29 +0300

    Integrate MongoDB with Entity Framework Core
    Added MongoDB configuration and integration with Entity Framework Core. Updated `Program.cs` to include custom extension methods for options and database services. Introduced `MongoOption` class for MongoDB settings. Created `AppDbContext` for MongoDB context management. Added entity configurations for `Category` and `Course` to map to MongoDB collections. Removed obsolete `Options` folder from project structure.

--- UdemyNewMicroservice.Catalog.Api/Program.cs
+++ UdemyNewMicroservice.Catalog.Api/Program.cs
@@ -1,7 +1,12 @@
+using UdemyNewMicroservice.Catalog.Api.Options;
+using UdemyNewMicroservice.Catalog.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOptionsExt();
+builder.Services.AddDatabaseServiceExt();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
